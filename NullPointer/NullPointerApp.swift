db7dc2651c334cd8dacbca213af76be29eca0b66
Bhanuteja74 <kbhanut@gmail.com>
2025-09-27 19:48:45 +0530

    Initial Commit

---
+++ NullPointer/NullPointerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NullPointerApp.swift
+//  NullPointer
+//
+//  Created by Bhanu Teja Kommi on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct NullPointerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
